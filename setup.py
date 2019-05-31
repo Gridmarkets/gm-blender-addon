e8fa242a6376d6d027a8ebeebba1460404621871
OllieDawes <olliedawes@gmail.com>
2019-05-31 12:20:29 +0100

    create setup.py which can package blender addon as .zip or as a plain folder

--- setup.py
+++ setup.py
@@ -1,6 +1,172 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-setup(name='gridmarkets_blender_plugin',
-      version='0.3.2',
-      py_modules=['gridmarkets_blender_plugin'],
-      )
+import glob
+import os
+import sys
+import shutil
+import subprocess
+import pathlib
+import zipfile
+
+from distutils import log
+from distutils.core import Command
+from distutils.command.install_egg_info import install_egg_info
+from setuptools import setup, find_packages
+
+
+# Global Constants
+HERE = pathlib.Path(__file__).resolve().parent
+ADDON_FOLDER = HERE / 'gridmarkets_blender_addon'
+DEFAULT_LIB_FOLDER = ADDON_FOLDER / 'lib'
+
+DIST_FOLDER = HERE / 'dist'
+DEFAULT_DIST_OUTPUT_LOCATION = (DIST_FOLDER / 'gridmarkets_blender_addon')
+DEFAULT_ZIP_OUTPUT_LOCATION = DEFAULT_DIST_OUTPUT_LOCATION.with_suffix('.zip')
+
+# import readme text for long description
+readme = open('README.md', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+# Package meta-data.
+NAME = 'gridmarkets_blender_addon'
+DESCRIPTION = 'Allows users to submit Blender jobs to the Gridmarkets render farm from within Blender.'
+LONG_DESCRIPTION = README_TEXT
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+URL = 'https://www.gridmarkets.com/'
+EMAIL = 'olliedawes@gmail.com'
+AUTHOR = 'Ollie Dawes'
+VERSION = '0.3.4'
+
+requirements = [
+    'https://gm-envoy-client-docs.netlify.com/downloads/gridmarkets_envoy_client-0.6.0-py2.py3-none-any.whl',
+    'blender-asset-tracer',
+]
+
+
+class BuildDependencies(Command):
+    """Downloads and builds the dependencies to the add-ons lib folder."""
+
+    description = "Downloads and builds the dependencies to the add-ons lib folder."
+    user_options = [
+        ('lib-path=', 'd', "the path the dependencies are stored for use by the add-on"),
+    ]
+
+    def initialize_options(self):
+        self.lib_path = None
+
+    def finalize_options(self):
+        # use default lib path unless one is explicitly provided by the user
+        self.lib_path = self.lib_path or DEFAULT_LIB_FOLDER
+
+    def run(self):
+        log.info('Installing packages to: %s', self.lib_path)
+
+        for package in requirements:
+            self.install_package(package)
+
+    def install_package(self, package):
+        """Installs wheel from PyPI to lib folder"""
+
+        log.info('Installing package: %s', package)
+
+        subprocess.call([sys.executable,
+                         "-m",
+                         "pip",
+                         "install",
+                         "-t", str(self.lib_path),
+                         package])
+
+
+class Bzip(Command):
+    """ Zips the add-on folder for distribution so that it can be installed by Blender. Overwrites any existing zip file
+    of the same name in the output folder.
+    """
+
+    description = "Zips the add-on folder for distribution so that it can be installed by Blender. Overwrites any" \
+                  "existing zip file of the same name in the output folder"
+    user_options = [
+        ('path=', 'd', "the path the zip file is output to"),
+    ]
+
+    def initialize_options(self):
+        self.path = None
+
+    def finalize_options(self):
+        # use default path unless one is explicitly provided by the user
+        self.path = self.path or DEFAULT_DIST_OUTPUT_LOCATION
+
+    def run(self):
+        # create the dist folder if it does not exist yet
+        DIST_FOLDER.mkdir(parents=True, exist_ok=True)
+
+        shutil.make_archive(
+            self.path, # zip file name
+            'zip',  # the archive format - or tar, bztar, gztar
+            root_dir=ADDON_FOLDER,  # root for archive - current working dir if None
+            base_dir=None)  # start archiving from here - cwd if None too
+
+
+class Fdist(Bzip):
+    """ Extracts the output of Bzip to either the dist folder or a provided location."""
+
+    description = "Extracts the output of Bzip to either the dist folder or a provided location."
+    user_options = [
+        ('path=', 'd', 'the path to copy the add-on folder to'),
+    ]
+
+    def initialize_options(self):
+        self.path = None
+
+    def finalize_options(self):
+        # use default path unless one is explicitly provided by the user
+        self.path = self.path or DEFAULT_DIST_OUTPUT_LOCATION
+
+    def run(self):
+
+        # delete the output folder if it already exists
+        if os.path.exists(self.path):
+            shutil.rmtree(self.path, ignore_errors=True)
+
+        # re-generate the zipped add-on
+        self.run_command('bzip')
+
+        print('Unzipping the add-on to {}.'.format(self.path))
+
+        with zipfile.ZipFile(DEFAULT_ZIP_OUTPUT_LOCATION, 'r') as zip_ref:
+            zip_ref.extractall(self.path)
+
+
+class AvoidEggInfo(install_egg_info):
+    """Makes sure the egg-info directory is NOT created.
+    If we skip this, the user's addon directory can be polluted by egg-info
+    directories, which Blender doesn't use anyway.
+    """
+
+    def run(self):
+        pass
+
+
+setup(
+    cmdclass={'bzip': Bzip,
+              'fdist': Fdist,
+              'wheels': BuildDependencies},
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages('.'),
+    data_files=[('gridmarkets_blender_plugin', ['README.md']),
+                ('gridmarkets_blender_plugin/icons', glob.glob('gridmarkets_blender_plugin/icons/*'))],
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Environment :: Plugins',
+        'Programming Language :: Python'
+    ]
+)
