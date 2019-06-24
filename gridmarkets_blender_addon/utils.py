742a7b4021f8c3ac85ed756f8585932be820de0f
OllieDawes <olliedawes@gmail.com>
2019-06-24 17:58:00 +0100

    add get_unique_id method

--- gridmarkets_blender_addon/utils.py
+++ gridmarkets_blender_addon/utils.py
@@ -72,6 +72,35 @@ def pack_blend_file(blend_file, target):
     p.wait()
 
 
+def get_unique_id(objects, attribute_name='id'):
+    """ Returns an integer ID that has not been used by any other object in the provided list.
+
+    :param objects: a list of existing objects that may or may not have an id attribute
+    :type objects: list
+    :param attribute_name: the attribute containing the id
+    :type attribute_name: str
+    :return: the new unique id
+    :rtype: int
+    """
+
+    used_ids = []
+
+    for object in objects:
+        id = object.id
+
+        if not isinstance(id, int):
+            continue
+
+        used_ids.append(id)
+
+    # find a number which has not been used yet
+    unused_id = 0
+    while unused_id in used_ids:
+        unused_id = unused_id + 1
+
+    return unused_id
+
+
 def create_unique_object_name(objects, attribute_name='name', name_prefix = ""):
     """ Creates a unique name of the form [name_prefix][x] where x is some unique positive integer.
 
