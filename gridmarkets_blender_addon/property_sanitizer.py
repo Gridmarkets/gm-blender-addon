542b968a69f35407f0e766299e1095cef32a8b51
OllieDawes <olliedawes@gmail.com>
2019-06-25 09:25:11 +0100

    move validation response into separate file

--- gridmarkets_blender_addon/property_sanitizer.py
+++ gridmarkets_blender_addon/property_sanitizer.py
@@ -3,44 +3,7 @@ import pathlib
 import uuid
 import constants
 
-
-class ValidationResponse():
-
-    def __init__(self, error_message=None):
-
-        self._error_message = error_message
-
-        if error_message == None:
-            self._is_valid = True
-        else:
-            self._is_valid = False
-
-    def is_valid(self):
-        """ Did the validation method pass
-
-        :return: is the validation response valid
-        :rtype: bool
-        """
-
-        return self._is_valid
-
-    def is_invalid(self):
-        """ Did the validation method fail
-
-        :return: opposite of is_valid
-        :rtype: bool
-        """
-
-        return not self.is_valid()
-
-    def get_error_message(self):
-        """ Getter
-
-        :return: The error message explaining why validation failed
-        :rtype: str
-        """
-        return self._error_message
-
+from validation_response import ValidationResponse
 
 class PropertySanitizer():
     """ Helper class which provides useful methods for getting and validating the values of user input fields"""
