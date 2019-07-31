0fd2f9fb4b2e5c1246e9d5ad0c7ea1415cdbcfe4
OllieDawes <olliedawes@gmail.com>
2019-07-31 14:33:15 +0100

    use empty layout

--- gridmarkets_blender_addon/panel_overloaders/tabs.py
+++ gridmarkets_blender_addon/panel_overloaders/tabs.py
@@ -1,5 +1,6 @@
 import bpy
 from utils_blender import addon_draw_condition
+from layouts.empty import draw_empty
 from .panel_overloader import PanelOverloader
 
 _instance = None
@@ -9,12 +10,8 @@ class TabsOverloader(PanelOverloader):
 
     def __init__(self):
         super().__init__(bpy.types.USERPREF_PT_tabs,
-                         TabsOverloader._draw_tabs,
+                         draw_empty,
                          overload_condition=addon_draw_condition)
-
-    @staticmethod
-    def _draw_tabs(self, context):
-        pass
 
 
 def register():
