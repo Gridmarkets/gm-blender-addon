40c98292d2902960d6ae0d01c1e5609aae0b602e
OllieDawes <olliedawes@gmail.com>
2019-09-09 17:46:35 +0100

    Create a property group for LogItems
    Blender requires that objects that are going to be shown in a list are stored using it's internal property group type.

---
+++ gridmarkets_blender_addon/blender_plugin/log_item/property_groups/log_item_props.py
@@ -0,0 +1,30 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+# GridMarkets Blender Add-on - A Blender Add-on for rendering scenes / files on the Grid Computing network GridMarkets
+# Copyright (C) 2019  GridMarkets
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import bpy
+
+
+class LogItemProps(bpy.types.PropertyGroup):
+
+    id: bpy.props.IntProperty(
+        name="id",
+        description="A arbitrary unique identifier generated locally to differentiate between log items",
+        options={'SKIP_SAVE', 'HIDDEN'}
+    )
