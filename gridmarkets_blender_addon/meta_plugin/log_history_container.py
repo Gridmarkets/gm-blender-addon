457ce3dac5ec43e125758ae008a1a48f225df587
OllieDawes <olliedawes@gmail.com>
2019-09-09 17:34:41 +0100

    Override generic ListContainer methods for better PyCharm type suggestions

--- gridmarkets_blender_addon/meta_plugin/log_history_container.py
+++ gridmarkets_blender_addon/meta_plugin/log_history_container.py
@@ -28,3 +28,33 @@ class LogHistoryContainer(ListContainer, LogItem):
 
     def __init__(self, log_item: typing.List[LogItem]):
         ListContainer.__init__(self, log_item)
+
+    def get_focused_item(self) -> typing.Optional[LogItem]:
+        return ListContainer.get_focused_item(self)
+
+    def focus_item(self, log_item: LogItem, clear_selection=True) -> None:
+        return ListContainer.focus_item(self, log_item, clear_selection=clear_selection)
+
+    def get_selected(self) -> typing.List[LogItem]:
+        return ListContainer.get_selected(self)
+
+    def get_all(self) -> typing.List[LogItem]:
+        return ListContainer.get_all(self)
+
+    def get_at(self, index: int) -> LogItem:
+        return ListContainer.get_at(self, index)
+
+    def append(self, log_item: LogItem, focus_new_item: bool = True) -> None:
+        return ListContainer.append(self, log_item, focus_new_item=focus_new_item)
+
+    def remove(self, log_item: LogItem) -> None:
+        return ListContainer.remove(self, log_item)
+
+    def contains(self, log_item: LogItem) -> bool:
+        return ListContainer.contains(self, log_item)
+
+    def get_index(self, log_item: LogItem) -> int:
+        return ListContainer.get_index(self, log_item)
+
+    def is_selected(self, log_item: LogItem):
+        return ListContainer.is_selected(self, log_item)
