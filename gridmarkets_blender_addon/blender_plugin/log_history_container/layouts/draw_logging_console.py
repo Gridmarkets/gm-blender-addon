8314efd6ed08ee10e110b7cb93d16645e4b780aa
OllieDawes <olliedawes@gmail.com>
2019-09-10 13:56:29 +0100

    Add logging display options to collapsible menu

--- gridmarkets_blender_addon/blender_plugin/log_history_container/layouts/draw_logging_console.py
+++ gridmarkets_blender_addon/blender_plugin/log_history_container/layouts/draw_logging_console.py
@@ -20,14 +20,30 @@
 
 
 def draw_logging_console(self, context):
+    from gridmarkets_blender_addon.blender_plugin.plugin_fetcher.plugin_fetcher import PluginFetcher
     from gridmarkets_blender_addon.blender_plugin.log_item.list_items.log_item_list import GRIDMARKETS_UL_log_item
+    from gridmarkets_blender_addon.blender_plugin.log_history_container.menus.display_options import \
+        GRIDMARKETS_MT_logging_display_options
+    from gridmarkets_blender_addon import constants
 
     layout = self.layout
-
     props = context.scene.props
     log_history_container_props = props.log_history_container
 
-    layout.template_list(GRIDMARKETS_UL_log_item.bl_idname, "",
+    plugin = PluginFetcher.get_plugin()
+    user_interface = plugin.get_user_interface()
+
+    show_dates = user_interface.show_log_dates()
+    show_times = user_interface.show_log_times()
+    show_names = user_interface.show_logger_names()
+
+    row = layout.row()
+
+    row.template_list(GRIDMARKETS_UL_log_item.bl_idname, "",
                       log_history_container_props, "log_history_items",
                       log_history_container_props, "focused_log_item",
                       rows=6, sort_lock=True)
+
+    col = row.column(align=True)
+    col.ui_units_x = 1
+    col.menu(GRIDMARKETS_MT_logging_display_options.bl_idname, icon=constants.ICON_COLLAPSEMENU, text="")
