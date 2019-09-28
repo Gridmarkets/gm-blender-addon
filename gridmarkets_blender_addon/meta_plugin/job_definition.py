33b0ee4536875d7cf50fa464a9a23438750a2c98
OllieDawes <olliedawes@gmail.com>
2019-09-28 05:28:16 +0100

    Add get_attribtutes method to JobDefinition class

--- gridmarkets_blender_addon/meta_plugin/job_definition.py
+++ gridmarkets_blender_addon/meta_plugin/job_definition.py
@@ -18,13 +18,13 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
-from typing import List
+import typing
 from gridmarkets_blender_addon.meta_plugin.job_attribute import JobAttribute
 
 
 class JobDefinition:
 
-    def __init__(self, id: str, display_name: str, attributes: List[JobAttribute]):
+    def __init__(self, id: str, display_name: str, attributes: typing.List[JobAttribute]):
         self._id = id
         self._display_name = display_name
         self._attributes = attributes
@@ -35,5 +35,12 @@ class JobDefinition:
     def get_display_name(self) -> str:
         return self._display_name
 
-    def get_attributes(self) -> List[JobAttribute]:
+    def get_attributes(self) -> typing.List[JobAttribute]:
         return self._attributes
+
+    def get_attribute_with_key(self, key: str) -> typing.Optional[JobAttribute]:
+        for attribute in self.get_attributes():
+            if attribute.get_key() == key:
+                return attribute
+
+        return None
