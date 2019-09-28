a38f29420b12d3591055a5acb231ec810f3099e9
OllieDawes <olliedawes@gmail.com>
2019-09-28 05:30:10 +0100

    Add operator for creating JobPreset

---
+++ gridmarkets_blender_addon/blender_plugin/job_preset_container/operators/create_new_job_preset.py
@@ -0,0 +1,87 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+# GridMarkets Blender Add-on - A Blender Add-on for rendering scenes / files on the Grid Computing network GridMarkets
+# Copyright (C) 2019  GridMarkets
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import bpy
+
+
+class GRIDMARKETS_OT_create_new_job_preset(bpy.types.Operator):
+    bl_idname = "gridmarkets.create_new_job_preset"
+    bl_label = "Create new job Preset"
+    bl_description = "Creates a new Job Preset and adds it to the Job Presets list"
+    bl_options = {"REGISTER", "UNDO"}
+
+    job_definition_id: bpy.props.StringProperty(
+        name="Job Definition Id",
+        description="The unique identifier for the job definition that is being used to create a job preset from"
+    )
+
+    def execute(self, context):
+        from gridmarkets_blender_addon.blender_plugin.plugin_fetcher.plugin_fetcher import PluginFetcher
+        from gridmarkets_blender_addon.meta_plugin.job_preset import JobPreset
+        from gridmarkets_blender_addon import utils, constants
+
+        plugin = PluginFetcher.get_plugin()
+        api_client = plugin.get_api_client()
+        api_schema = api_client.get_api_schema()
+        job_definitions = api_schema.get_job_definitions()
+
+        if self.job_definition_id is None:
+            self.report({'ERROR_INVALID_INPUT'}, "Job Definition ID must not be 'None'.")
+            return {'FINISHED'}
+
+        for job_definition in job_definitions:
+            if job_definition.get_definition_id() == self.job_definition_id:
+                job_preset_container = plugin.get_preferences_container().get_job_preset_container()
+
+                job_preset_items = bpy.context.scene.props.job_preset_container.items
+
+                job_preset_id = utils.get_unique_id(job_preset_items)
+                name = utils.create_unique_object_name(job_preset_items, name_prefix=(
+                            job_definition.get_display_name() + "_Job_Preset_").replace(' ', '_'))
+                job_preset = JobPreset(name, job_preset_id, job_definition)
+
+                job_preset_container.append(job_preset)
+                return {'FINISHED'}
+        else:
+            self.report({'ERROR_INVALID_INPUT'},
+                        "Job Definition ID must match a know Job Definition. It was '" + str(
+                            self.job_definition_id) + "'.")
+            return {'FINISHED'}
+
+
+classes = (
+    GRIDMARKETS_OT_create_new_job_preset,
+)
+
+
+def register():
+    from bpy.utils import register_class
+
+    # register classes
+    for cls in classes:
+        register_class(cls)
+
+
+def unregister():
+    from bpy.utils import unregister_class
+
+    # unregister classes
+    for cls in reversed(classes):
+        unregister_class(cls)
