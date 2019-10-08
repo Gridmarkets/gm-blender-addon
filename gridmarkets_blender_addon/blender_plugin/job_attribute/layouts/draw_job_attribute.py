c58d11ffd010e847a57ea69a0b67ae5ff2f0add7
OllieDawes <olliedawes@gmail.com>
2019-10-08 12:17:03 +0100

    Create method for drawing a job attribute

---
+++ gridmarkets_blender_addon/blender_plugin/job_attribute/layouts/draw_job_attribute.py
@@ -0,0 +1,95 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+# GridMarkets Blender Add-on - A Blender Add-on for rendering scenes / files on the Grid Computing network GridMarkets
+# Copyright (C) 2019  GridMarkets
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+from gridmarkets_blender_addon.meta_plugin.job_attribute import JobAttribute
+
+
+def draw_job_attribute(self, context, job_preset, job_attribute: JobAttribute, col1, col2, col3):
+    from types import SimpleNamespace
+    from gridmarkets_blender_addon.blender_plugin.job_attribute.operators.set_inference_source import \
+        GRIDMARKETS_OT_set_inference_source
+    from gridmarkets_blender_addon.layouts.draw_frame_range_container import draw_frame_range_container
+    from gridmarkets_blender_addon.meta_plugin.attribute_inference_source import AttributeInferenceSource
+    from gridmarkets_blender_addon.meta_plugin.attribute import AttributeType
+    from gridmarkets_blender_addon.meta_plugin.attribute_types import StringSubtype
+    from gridmarkets_blender_addon.blender_plugin.job_preset.job_preset import JobPreset
+
+    scene = context.scene
+    props = scene.props
+
+    job_preset_props = getattr(scene, job_preset.get_prop_id())
+
+    attribute = job_attribute.get_attribute()
+    attribute_type = attribute.get_type()
+
+    if attribute_type == AttributeType.NULL:
+        return
+
+    inference_sources = job_attribute.get_inference_sources()
+    inference_source = getattr(job_preset_props, JobPreset.INFERENCE_SOURCE_KEY + attribute.get_key())
+
+    display_name_row = col1.row()
+    display_name_row.label(text=attribute.get_display_name() + ":")
+
+    input_row = col2.row()
+
+    if inference_source == AttributeInferenceSource.CONSTANT.value:
+        input_row.prop(job_preset_props, attribute.get_key(), text="")
+        input_row.enabled = False
+
+    elif inference_source == AttributeInferenceSource.USER_DEFINED.value:
+
+        if attribute.get_type() == AttributeType.STRING and attribute.get_subtype() == StringSubtype.FRAME_RANGES.value:
+            draw_frame_range_container(SimpleNamespace(layout=input_row),
+                                       context,
+                                       'context.scene.' + job_preset.get_prop_id(),
+                                       attribute.get_key() + '_collection',
+                                       attribute.get_key() + '_focused')
+        else:
+            input_row.prop(job_preset_props, attribute.get_key(), text="")
+
+    elif inference_source == AttributeInferenceSource.PROJECT.value:
+        input_row.prop(props, "project_defined", text="")
+        input_row.enabled = False
+
+    inference_buttons = col3.row(align=True)
+    inference_buttons.alignment = "RIGHT"
+
+    def draw_inference_source_button(operator, label, expected_inference_source: str):
+        set_inference_source = getattr(job_preset_props, JobPreset.INFERENCE_SOURCE_KEY + attribute.get_key())
+        depress = set_inference_source == expected_inference_source
+
+        sub = inference_buttons.row(align=True)
+        sub.alignment = "LEFT"
+        sub.ui_units_x = 1
+        sub.enabled = expected_inference_source in inference_sources
+        op = sub.operator(operator.bl_idname, text=label if sub.enabled else " ", depress=depress)
+        op.inference_source = expected_inference_source
+        op.job_preset_prop_id = job_preset.get_prop_id()
+        op.job_attribute_key = attribute.get_key()
+
+    draw_inference_source_button(GRIDMARKETS_OT_set_inference_source, "U",
+                                 AttributeInferenceSource.USER_DEFINED.value)
+
+    draw_inference_source_button(GRIDMARKETS_OT_set_inference_source, "P",
+                                 AttributeInferenceSource.PROJECT.value)
+
+    draw_inference_source_button(GRIDMARKETS_OT_set_inference_source, "C",
+                                 AttributeInferenceSource.CONSTANT.value)
