b8e62c15ad01f2f4fdce534f1bd34e47098a2d33
OllieDawes <olliedawes@gmail.com>
2019-11-26 16:59:37 +0000

    Add inference source help popup
    Adds a popup menu which describes the different inference source types.

--- gridmarkets_blender_addon/blender_plugin/job_preset/layouts/draw_job_preset.py
+++ gridmarkets_blender_addon/blender_plugin/job_preset/layouts/draw_job_preset.py
@@ -18,6 +18,58 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+import bpy
+from gridmarkets_blender_addon import constants
+
+
+class GRIDMARKETS_OT_open_inference_source_help(bpy.types.Operator):
+    bl_idname = "gridmarkets.open_inference_source_help"
+    bl_label = "Open Inference Source Help"
+
+    def invoke(self, context, event):
+        from gridmarkets_blender_addon.meta_plugin.inference_source import InferenceSource
+
+        def draw_help(self, layout):
+            layout = self.layout
+            layout.scale_y = 0.8
+
+            layout.alignment = "EXPAND"
+            layout.label(text="Inference Source Help", icon=constants.ICON_INFO)
+            layout.label(text="Some Job Preset attributes let you choose between different inference sources. The possible source types are:")
+            layout.separator()
+
+            inference_sources = InferenceSource.get_all_inference_source_types()
+            for inference_source in inference_sources:
+                layout.label(text=inference_source.get_display_name() + ':')
+                description_row = layout.row()
+                description_row.enabled = False
+                description_row.label(text=inference_source.get_description())
+                layout.separator()
+
+        context.window_manager.popover(draw_help, ui_units_x=30)
+        return {'FINISHED'}
+
+
+classes = (
+    GRIDMARKETS_OT_open_inference_source_help,
+)
+
+
+def register():
+    from bpy.utils import register_class
+
+    # register classes
+    for cls in classes:
+        register_class(cls)
+
+
+def unregister():
+    from bpy.utils import unregister_class
+
+    # unregister classes
+    for cls in reversed(classes):
+        unregister_class(cls)
+
 
 def draw_job_preset(self, context):
     from gridmarkets_blender_addon import constants
@@ -65,10 +117,20 @@ def draw_job_preset(self, context):
 
         columns = _get_columns(col)
         columns[0].label(text="Attribute Name")
+        columns[0].enabled = False
+
         columns[1].label(text="Input Field")
-        columns[2].label(text="Source")
-        for column in columns:
-            column.enabled = False
+        columns[1].enabled = False
+
+        source_header_row = columns[2].row(align=True)
+        source_header_row_label = source_header_row.row()
+        source_header_row_label.enabled = False
+        source_header_row_label.label(text="Source")
+
+        source_header_row_operator = source_header_row.row()
+        source_header_row_operator.operator(GRIDMARKETS_OT_open_inference_source_help.bl_idname, text="", icon=constants.ICON_INFO, emboss=False)
+
+        col.separator()
 
         for job_attribute in attributes:
             if job_attribute.get_attribute().get_type() == AttributeType.NULL:
@@ -93,28 +155,3 @@ def draw_job_preset(self, context):
         row.alignment = "RIGHT"
         row.label(text="Show Hidden Job Preset Attributes")
         row.prop(scene.props.user_interface, "show_hidden_job_preset_attributes", text="")
-
-        col.separator()
-        col.label(text="Some Job Preset attributes let you choose between different sources. These Sources Include:",
-                  icon=constants.ICON_INFO)
-
-        indent = " " * 8
-
-        def _draw_source_hint(source_name: str, source_description: str):
-            row = col.row(align=True)
-
-            sub = row.row(align=True)
-            sub.ui_units_x = 10
-
-            sub.alignment="LEFT"
-            sub.label(text=indent + "- " + source_name + ":")
-
-            sub = row.row(align=True)
-            sub.alignment="EXPAND"
-            sub.enabled = False
-            sub.label(text=source_description)
-
-        _draw_source_hint("Application (A)", "The value is inferred from the application.")
-        _draw_source_hint("User Input Field (U)", "The value is read from the provided input field.")
-        _draw_source_hint("Project Inferred Value (P)", "The value will be read from the project's attributes when you submit.")
-        _draw_source_hint("Constant (C)","Uses a pre-defined constant value.")
