f5744b96fcb8d1ffe6ba0b35f9be02877b9b04b7
OllieDawes <olliedawes@gmail.com>
2020-04-28 10:55:25 +0100

    Add missing unregister methods to operator files

--- gridmarkets_blender_addon/blender_plugin/remote_project/operators/open_remote_project_definition_popup.py
+++ gridmarkets_blender_addon/blender_plugin/remote_project/operators/open_remote_project_definition_popup.py
@@ -55,3 +55,11 @@ def register():
     # register classes
     for cls in classes:
         register_class(cls)
+
+
+def unregister():
+    from bpy.utils import unregister_class
+
+    # unregister classes
+    for cls in reversed(classes):
+        unregister_class(cls)
