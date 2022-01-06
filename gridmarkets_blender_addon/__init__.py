2b641af369aede09b75e596f94d80ebd78b24cc0
haikü <olliedawes@gmail.com>
2022-01-06 11:32:21 +0000

    Add debug menu
    Adds a separate top bar menu item that only shows in debug mode containing operators that do the following actions:
    - Print debug status
    - Raise window
    - Stop Submit2gm

--- gridmarkets_blender_addon/__init__.py
+++ gridmarkets_blender_addon/__init__.py
@@ -36,9 +36,19 @@ import os
 import sys
 import typing
 
+PARENT_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_DIR_DIR = PARENT_DIR
+
+LOGGER_NAME = 'gridmarkets_blender_addon'
+PRODUCT = 'blender'
+META_INFO = 'gridmarkets_blender_addon version: {}\nblender version: {}'.format(str(bl_info.get('version', '')),
+                                                                                str(bpy.app.version))
+DEBUG_MODE = os.path.exists(os.path.join(PARENT_DIR, 'debug'))
+
+
 import configparser
 config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
+config.read(os.path.join(CONFIG_DIR_DIR, 'config.ini'))
 ENVOY_PYTHON_PATH = config['default']['ENVOY_PYTHON_PATH']
 if config.has_section('submit2gm'):
     SUBMIT2GM_PATH = config.get('submit2gm', 'SUBMIT2GM_PATH')
@@ -50,10 +60,20 @@ ICONS_FOLDER = "icons"
 GRIDMARKETS_LOGO_ID = "gridmarkets_logo"
 GM_GREYSCALE_LOGO = "logo_greyscale.png"
 MAIN_COLLECTION_ID = "main"
+
+# Operator constants
 OPERATOR_OPEN_SUBMIT2GM_ID_NAME = "gridmarkets.open_submit2gm"
 OPERATOR_OPEN_SUBMIT2GM_LABEL = "Open Submit2gm"
-TOP_BAR_LABEL = 'Open GridMarkets add-on'
+OPERATOR_PRINT_DEBUG_STATUS_ID_NAME = "gridmarkets.print_debug_status"
+OPERATOR_PRINT_DEBUG_STATUS_LABEL = "Print debug status"
+OPERATOR_RAISE_WINDOW_ID_NAME = "gridmarkets.raise_window"
+OPERATOR_RAISE_WINDOW_LABEL = "Raise window"
+OPERATOR_STOP_SUBMIT2GM_ID_NAME = "gridmarkets.stop_submit2gm"
+OPERATOR_STOP_SUBMIT2GM_LABEL = "Stop Submit2gm"
+MENU_DEBUG_ID_NAME = "GRIDMARKETS_MT_gm_debug_menu"
+MENU_DEBUG_LABEL = "GM Debug"
 
+TOP_BAR_LABEL = 'Open GridMarkets add-on'
 
 # pre-append the lib folder to sys.path so local dependencies can be found. Pre-append so that these paths take
 # precedence over blender's existing packed modules which can be different versions
@@ -145,6 +165,9 @@ def draw_top_bar(self, context: bpy.types.Context) -> None:
     self.layout.emboss = "PULLDOWN_MENU"
     self.layout.operator(OPERATOR_OPEN_SUBMIT2GM_ID_NAME, icon_value=iconGM.icon_id, text=TOP_BAR_LABEL)
 
+    if DEBUG_MODE:
+        self.layout.menu(MENU_DEBUG_ID_NAME)
+
 
 @bpy.app.handlers.persistent
 def process_qt_events() -> float:
@@ -183,6 +206,21 @@ def evaluate_code(code: str, return_variables: typing.List[str]):
     return results
 
 
+def import_submit2gm():
+    # Get the location of the submit2gm python package
+    submit2gm_package_path = os.path.join(os.path.dirname(SUBMIT2GM_PATH), 'submit2gm')
+
+    # Import the submit2gm package
+    submit2gm_init_path = os.path.join(submit2gm_package_path, "__init__.py")
+
+    from importlib import util as importlib_util
+    spec = importlib_util.spec_from_file_location('submit2gm', submit2gm_init_path)
+    module = importlib_util.module_from_spec(spec)
+
+    sys.modules[spec.name] = module
+    spec.loader.exec_module(module)
+
+
 class GRIDMARKETS_OT_open_submit2gm(bpy.types.Operator):
     """ Opens the GridMarkets Submit2gm app."""
 
@@ -190,39 +228,79 @@ class GRIDMARKETS_OT_open_submit2gm(bpy.types.Operator):
     bl_label = OPERATOR_OPEN_SUBMIT2GM_LABEL
     bl_options = {'REGISTER'}
 
-    def import_submit2gm(self):
-        # Get the location of the submit2gm python package
-        submit2gm_package_path = os.path.join(os.path.dirname(SUBMIT2GM_PATH), 'submit2gm')
-
-        # Import the submit2gm package
-        submit2gm_init_path = os.path.join(submit2gm_package_path, "__init__.py")
-
-        from importlib import util as importlib_util
-        spec = importlib_util.spec_from_file_location('submit2gm', submit2gm_init_path)
-        module = importlib_util.module_from_spec(spec)
-
-        sys.modules[spec.name] = module
-        spec.loader.exec_module(module)
-
     def execute(self, context: bpy.types.Context):
-        self.import_submit2gm()
+        import_submit2gm()
 
         from submit2gm.command_port_server_utils import start_command_port_server
-        envoy_python_path = ENVOY_PYTHON_PATH
-        logger_name = 'gridmarkets_blender_addon'
-        product = 'blender'
-        meta_info = 'gridmarkets_blender_addon version: ' + str(bl_info.get('version', '')) + '\n'
-        meta_info += 'blender version: ' + str(bpy.app.version)
-        evaluate_code_cb = evaluate_code
-        debug_mode = True
-
-        start_command_port_server(envoy_python_path, logger_name, product, meta_info, evaluate_code_cb, debug_mode)
+        start_command_port_server(ENVOY_PYTHON_PATH,
+                                  LOGGER_NAME,
+                                  PRODUCT,
+                                  META_INFO,
+                                  evaluate_code,
+                                  DEBUG_MODE)
 
         return {"FINISHED"}
 
 
+class GRIDMARKETS_OT_print_debug_status(bpy.types.Operator):
+    """ Prints debug information surrounding the command_port_utils state."""
+
+    bl_idname = OPERATOR_PRINT_DEBUG_STATUS_ID_NAME
+    bl_label = OPERATOR_PRINT_DEBUG_STATUS_LABEL
+    bl_options = {'REGISTER', 'INTERNAL'}
+
+    def execute(self, context: bpy.types.Context):
+        import_submit2gm()
+        from submit2gm.command_port_server_utils import print_debug_status
+        print_debug_status()
+        return {"FINISHED"}
+
+
+class GRIDMARKETS_OT_raise_window_status(bpy.types.Operator):
+    """ Raises the GridMarkets Submit2gm app window."""
+
+    bl_idname = OPERATOR_RAISE_WINDOW_ID_NAME
+    bl_label = OPERATOR_RAISE_WINDOW_LABEL
+    bl_options = {'REGISTER', 'INTERNAL'}
+
+    def execute(self, context: bpy.types.Context):
+        import_submit2gm()
+        from submit2gm.command_port_server_utils import raise_window
+        raise_window()
+        return {"FINISHED"}
+
+
+class GRIDMARKETS_OT_stop_submit2gm_status(bpy.types.Operator):
+    """ Stops the GridMarkets Submit2gm app."""
+
+    bl_idname = OPERATOR_STOP_SUBMIT2GM_ID_NAME
+    bl_label = OPERATOR_STOP_SUBMIT2GM_LABEL
+    bl_options = {'REGISTER', 'INTERNAL'}
+
+    def execute(self, context: bpy.types.Context):
+        import_submit2gm()
+        from submit2gm.command_port_server_utils import stop_submit2gm
+        stop_submit2gm()
+        return {"FINISHED"}
+
+
+class GRIDMARKETS_MT_debug_menu(bpy.types.Menu):
+    bl_idname = MENU_DEBUG_ID_NAME
+    bl_label = MENU_DEBUG_LABEL
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator(OPERATOR_PRINT_DEBUG_STATUS_ID_NAME)
+        layout.operator(OPERATOR_RAISE_WINDOW_ID_NAME)
+        layout.operator(OPERATOR_STOP_SUBMIT2GM_ID_NAME)
+
+
 classes = (
     GRIDMARKETS_OT_open_submit2gm,
+    GRIDMARKETS_OT_print_debug_status,
+    GRIDMARKETS_OT_raise_window_status,
+    GRIDMARKETS_OT_stop_submit2gm_status,
+    GRIDMARKETS_MT_debug_menu
 )
 
 
